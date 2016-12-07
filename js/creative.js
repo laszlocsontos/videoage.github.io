661ceacbd63f69709c56287183fbb80dd2701a8d
Antonio Trento <antoniotrento83@gmail.com>
2016-12-07 20:21:46 +0100

    s

--- js/creative.js
+++ js/creative.js
@@ -47,7 +47,6 @@
 
 })(jQuery); // End of use strict
 
-
 /* Light YouTube Embeds by @labnol */
 /* Web: http://labnol.org/?p=27941 */
 
@@ -65,14 +64,14 @@ document.addEventListener("DOMContentLoaded",
     });
 
 function labnolThumb(id) {
-    var thumb = '<img src="https://i.ytimg.com/vi/ePbKGoIGAXY/hqdefault.jpg">',
+    var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
         play = '<div class="play"></div>';
     return thumb.replace("ID", id) + play;
 }
 
 function labnolIframe() {
     var iframe = document.createElement("iframe");
-    var embed = "https://www.youtube.com/embed/ePbKGoIGAXY?autoplay=1";
+    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
     iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("allowfullscreen", "1");
