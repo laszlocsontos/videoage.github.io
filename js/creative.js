508f3ca592c1a0fec9b3376cd92a2cd26720f328
Antonio Trento <antoniotrento83@gmail.com>
2016-12-07 20:15:32 +0100

    s

--- js/creative.js
+++ js/creative.js
@@ -65,14 +65,14 @@ document.addEventListener("DOMContentLoaded",
     });
 
 function labnolThumb(id) {
-    var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+    var thumb = '<img src="https://i.ytimg.com/vi/ePbKGoIGAXY/hqdefault.jpg">',
         play = '<div class="play"></div>';
     return thumb.replace("ID", id) + play;
 }
 
 function labnolIframe() {
     var iframe = document.createElement("iframe");
-    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+    var embed = "https://www.youtube.com/embed/ePbKGoIGAXY?autoplay=1";
     iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("allowfullscreen", "1");
